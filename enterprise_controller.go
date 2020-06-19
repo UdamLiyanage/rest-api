d78ba6b6f6c4e440d382b3c187e70cbdd5a2277a
UdamLiyanage <udam1998@gmail.com>
2020-06-19 19:35:22 +0530

    Enterprise model routes and controller stubs

---
+++ enterprise_controller.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/labstack/echo/v4"
+
+func getEnterprises(c echo.Context) error {
+	return c.JSON(200, nil)
+}
+
+func getEnterprise(c echo.Context) error {
+	return c.JSON(200, nil)
+}
+
+func createEnterprise(c echo.Context) error {
+	return c.JSON(200, nil)
+}
+
+func updateEnterprise(c echo.Context) error {
+	return c.JSON(200, nil)
+}
+
+func deleteEnterprise(c echo.Context) error {
+	return c.JSON(200, nil)
+}
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func setupRouter() *echo.Echo {
+	e := echo.New()
+	e.Use(middleware.RequestID())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "time=${time_rfc3339} method=${method}, uri=${uri}, status=${status} path=${path} latency=${latency_human}\n",
+	}))
+
+	e.Use(middleware.Recover())
+
+	e.GET("/enterprises", getEnterprises)
+	e.GET("/enterprises/:id", getEnterprise)
+
+	e.POST("/enterprises", createEnterprise)
+
+	e.PUT("/enterprises/:id", updateEnterprise)
+
+	e.DELETE("/enterprises/:id", deleteEnterprise)
+
+	return e
+}
 
 func main() {
-	fmt.Println("Main Function")
+	r := setupRouter()
+	r.Logger.Fatal(r.Start(":8000"))
 }
