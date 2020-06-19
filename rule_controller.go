77971559f503665717496fae1c9cd5d1c6f2ca58
UdamLiyanage <udam1998@gmail.com>
2020-06-19 22:59:00 +0530

    rule_controller.go.go CRUD core

--- rule_controller.go
+++ rule_controller.go
@@ -3,21 +3,44 @@ package main
 import "github.com/labstack/echo/v4"
 
 func getRules(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: ruleCollection,
+	}
+	rules := config.read()
+	return c.JSON(200, rules)
 }
 
 func getRule(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: ruleCollection,
+	}
+	rule := config.read()
+	return c.JSON(200, rule)
 }
 
 func createRule(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: ruleCollection,
+	}
+	rule := config.create()
+	return c.JSON(201, rule)
 }
 
 func updateRule(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: ruleCollection,
+	}
+	rule := config.read()
+	return c.JSON(200, rule)
 }
 
 func deleteRule(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: ruleCollection,
+	}
+	status := config.delete()
+	if status {
+		return c.JSON(401, nil)
+	}
+	return c.JSON(500, nil)
 }
