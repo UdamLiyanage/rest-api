e0dbe6bea7c8799b048a526f4ea9e2e1c899d1b2
UdamLiyanage <udam1998@gmail.com>
2020-06-19 22:53:12 +0530

    device_controller.go CRUD core

--- device_controller.go
+++ device_controller.go
@@ -3,21 +3,44 @@ package main
 import "github.com/labstack/echo/v4"
 
 func getDevices(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: deviceCollection,
+	}
+	devices := config.read()
+	return c.JSON(200, devices)
 }
 
 func getDevice(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: deviceCollection,
+	}
+	device := config.read()
+	return c.JSON(200, device)
 }
 
 func createDevice(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: deviceCollection,
+	}
+	device := config.create()
+	return c.JSON(201, device)
 }
 
 func updateDevice(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: deviceCollection,
+	}
+	device := config.update()
+	return c.JSON(200, device)
 }
 
 func deleteDevice(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: deviceCollection,
+	}
+	status := config.delete()
+	if status {
+		return c.JSON(401, nil)
+	}
+	return c.JSON(500, nil)
 }
