958c8080cdab2147dbd482a17cf7a15ae93ef783
UdamLiyanage <udam1998@gmail.com>
2020-06-19 23:00:41 +0530

    user_controller.go CRUD core

--- user_controller.go
+++ user_controller.go
@@ -3,21 +3,44 @@ package main
 import "github.com/labstack/echo/v4"
 
 func getUsers(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: userCollection,
+	}
+	users := config.read()
+	return c.JSON(200, users)
 }
 
 func getUser(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: userCollection,
+	}
+	user := config.read()
+	return c.JSON(200, user)
 }
 
 func createUser(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: userCollection,
+	}
+	user := config.create()
+	return c.JSON(201, user)
 }
 
 func updateUser(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: userCollection,
+	}
+	user := config.read()
+	return c.JSON(200, user)
 }
 
 func deleteUser(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: userCollection,
+	}
+	status := config.delete()
+	if status {
+		return c.JSON(401, nil)
+	}
+	return c.JSON(500, nil)
 }
