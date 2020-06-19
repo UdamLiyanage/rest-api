32be494df49a6bdf823e96afef9d4ffbb0584c10
UdamLiyanage <udam1998@gmail.com>
2020-06-19 22:57:02 +0530

    enterprise_controller.go CRUD core

--- enterprise_controller.go
+++ enterprise_controller.go
@@ -3,21 +3,44 @@ package main
 import "github.com/labstack/echo/v4"
 
 func getEnterprises(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: enterpriseCollection,
+	}
+	enterprises := config.read()
+	return c.JSON(200, enterprises)
 }
 
 func getEnterprise(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: enterpriseCollection,
+	}
+	enterprise := config.read()
+	return c.JSON(200, enterprise)
 }
 
 func createEnterprise(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: enterpriseCollection,
+	}
+	enterprise := config.create()
+	return c.JSON(201, enterprise)
 }
 
 func updateEnterprise(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: enterpriseCollection,
+	}
+	enterprise := config.read()
+	return c.JSON(200, enterprise)
 }
 
 func deleteEnterprise(c echo.Context) error {
-	return c.JSON(200, nil)
+	var config = Configuration{
+		Collection: enterpriseCollection,
+	}
+	status := config.delete()
+	if status {
+		return c.JSON(401, nil)
+	}
+	return c.JSON(500, nil)
 }
