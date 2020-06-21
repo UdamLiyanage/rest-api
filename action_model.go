9e4b69832b861af7bf7ab71132f2571bf1324573
UdamLiyanage <udam1998@gmail.com>
2020-06-21 09:05:09 +0530

    Action Model

---
+++ action_model.go
@@ -0,0 +1,18 @@
+package main
+
+import "encoding/json"
+
+func UnmarshalAction(data []byte) (Action, error) {
+	var r Action
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Action) MarshalAction() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Action struct {
+	Type          string      `json:"type"`
+	Configuration interface{} `json:"configuration"`
+}
