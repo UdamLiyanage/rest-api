7bcad041ecd933120230504e3677f454a7e2572d
UdamLiyanage <udam1998@gmail.com>
2020-06-21 09:07:16 +0530

    Rule Model

---
+++ rule_model.go
@@ -0,0 +1,25 @@
+package main
+
+import "encoding/json"
+
+func UnmarshalRule(data []byte) (Rule, error) {
+	var r Rule
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Rule) MarshalRule() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Rule struct {
+	ActionID   string    `json:"action_id"`
+	ActionType string    `json:"action_type"`
+	Rule       RuleClass `json:"rule"`
+}
+
+type RuleClass struct {
+	Parameter string      `json:"parameter"`
+	Condition string      `json:"condition"`
+	Value     interface{} `json:"value"`
+}
