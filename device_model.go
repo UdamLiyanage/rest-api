b65809e853376145b4a4ef1086fe37bbaa8d7412
UdamLiyanage <udam1998@gmail.com>
2020-06-21 09:03:16 +0530

    Device Model

---
+++ device_model.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+)
+
+func UnmarshalDevice(data []byte) (Device, error) {
+	var r Device
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Device) MarshalDevice() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Device struct {
+	ID                 string    `json:"_id"`
+	Serial             string    `json:"serial"`
+	Urn                string    `json:"urn"`
+	Name               string    `json:"name"`
+	FirmwareVersion    string    `json:"firmware_version"`
+	LastFirmwareUpdate string    `json:"last_firmware_update"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
+}
