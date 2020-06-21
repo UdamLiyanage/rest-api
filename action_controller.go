a1ae01f2e7d5382ef731ab6ef816ad78fb6da6de
UdamLiyanage <udam1998@gmail.com>
2020-06-21 20:05:06 +0530

    action_controller create action

--- action_controller.go
+++ action_controller.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"io/ioutil"
+)
 
 func getActions(c echo.Context) error {
 	var crud Operations = Configuration{
@@ -22,8 +25,19 @@ func createAction(c echo.Context) error {
 	var crud Operations = Configuration{
 		Collection: actionCollection,
 	}
-	action := crud.Create()
-	return c.JSON(201, action)
+	body, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		panic(err)
+	}
+	action, err := UnmarshalAction(body)
+	if err != nil {
+		panic(err)
+	}
+	response, err := crud.Create(action)
+	if err != nil {
+		panic(err)
+	}
+	return c.JSON(201, response)
 }
 
 func updateAction(c echo.Context) error {
