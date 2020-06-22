1c38d347fad44c73de43ff316660c1f5436bd42f
UdamLiyanage <udam1998@gmail.com>
2020-06-22 13:00:30 +0530

    Attach Device Owner function in device_ownership_controller

---
+++ device_ownership_controller.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func attachDeviceOwner(c echo.Context) error {
+	var (
+		crud Operations = Configuration{
+			Collection: deviceCollection,
+		}
+		deviceOwnership DeviceOwnership
+	)
+	err := json.NewDecoder(c.Request().Body).Decode(&deviceOwnership)
+	if err != nil {
+		panic(err)
+	}
+	response, err := crud.Update(c.Param("urn"), bson.M{
+		"$set": bson.M{
+			"owner_urn": deviceOwnership.OwnerUrn,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	if response.MatchedCount == 0 {
+		return c.JSON(404, "document with urn not found")
+	} else {
+		if response.ModifiedCount == 0 {
+			return c.JSON(304, "document not modified")
+		}
+	}
+	return c.JSON(200, deviceOwnership)
+}
